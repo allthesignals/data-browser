daeb6dfa481f1e7ff86890cb15f042a3838ccca0
allthesignals <wilburnforce@gmail.com>
2018-01-24 23:56:15 -0500

    Use local config file in public folder

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -4,9 +4,14 @@ export default DS.JSONSerializer.extend({
   keyForAttribute(key) {
     return Ember.String.decamelize(key);
   },
-  primaryKey: 'cartodb_id',
+  primaryKey: 'id',
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    payload = payload.rows;
-    return this._super(store, primaryModelClass, payload, id, requestType);
+    const newPayload = payload.map((row, i) => {
+      const mutatedRow = row;
+      mutatedRow.id = i;
+      return mutatedRow;
+    });
+
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
   }
 });
--- config/environment.js
+++ config/environment.js
@@ -6,11 +6,10 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
-    dataBrowserIndex: 'https://mapc-admin.carto.com/api/v2/sql?q=select%20*%20from%20table_data_browser%20where%20schemaname=\'tabular\'%20and%20active=\'Y\'',
+    dataBrowserIndex: 'configuration.json',
     dataBrowserEndpoint: 'https://mapc-admin.carto.com/api/v2/sql?q=',
     spatialJoinFields: [  { field: 'ct10_id', table: 'census_2010_tracts'}, 
                           { field: 'muni_id', table: 'ma_municipalities'}
-                          // { field: 'bg10_id', table: ''} 
                         ],
     EmberENV: {
       FEATURES: {
