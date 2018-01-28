8dcc7df10aa1151f867efbe56718168a1070e9a7
allthesignals <wilburnforce@gmail.com>
2018-01-28 17:17:29 -0500

    Revert "Change env file"
    This reverts commit b718f09beeff3f2a6ff3b2e649093a952b06d82b.

--- config/environment.js
+++ config/environment.js
@@ -53,8 +53,6 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.rootURL = '/data-browser/';
-    ENV.locationType = 'hash';
-    ENV.rootURL = '/data-browser/';
 
   }
 
