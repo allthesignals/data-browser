52e23905113f0ab212d5cb308677f985946f54ad
allthesignals <wilburnforce@gmail.com>
2018-02-01 18:18:13 -0500

    Includes semantic original
    This allows for sophisticated overriddes of semantic UI

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,16 @@ module.exports = function(environment) {
       }
     },
 
+    googleFonts: [
+      'Montserrat:300,400,700'
+    ],
+
+    // Set or update content security policies
+    contentSecurityPolicy: {
+      'font-src': "'self' fonts.gstatic.com",
+      'style-src': "'self' fonts.googleapis.com"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
